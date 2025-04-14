e7e9acaca9284e3babae7fad53310b05fd1636ac
castlexyz <jonescastle@icloud.com>
2025-04-14 14:45:35 -0400

    Update archbase.sh

--- archbase.sh
+++ archbase.sh
@@ -196,6 +196,8 @@ hwclock --systohc
 
 # ---- localization ----
 sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+sudo locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
 
 # ---- network ----

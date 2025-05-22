b589ca714c21f4682217a3951a027d6dce41f95a
castlexyz <jonescastle@icloud.com>
2025-05-22 14:40:18 -0400

    Update archbase.sh

--- archbase.sh
+++ archbase.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+
+sudo clear # for sudo
+
+
 ###################################################################
 #      _                           _                 _            #
 #   __| | ___ _ __   ___ _ __   __| | ___ _ __   ___(_) ___  ___  #
@@ -7,91 +12,174 @@
 #  \__,_|\___| .__/ \___|_| |_|\__,_|\___|_| |_|\___|_|\___||___/ #
 #            |_|                                                  #
 ###################################################################
+if ! command -v pacman; then
+    echo "! This script must be ran on arch-linux"
+    exit 1
+fi
+
+
 platform_size="$(cat /sys/firmware/efi/fw_platform_size)"
 if [[ "${platform_size}" != "64" ]] && [[ "${platform_size}" != "32" ]]; then
-    echo "[ This script requires the UEFI boot mode. ]"
-    exit 0
+    echo "! This script requires the UEFI boot mode"
+    exit 1
 fi
+
+
 if ! ping -c 1 archlinux.org > /dev/null; then
-    echo "[ This script requires the internet. Use 'iwd' for Wifi. ]"
-    exit 0
+    echo "! This script requires the internet (use 'iwd' for wifi)"
+    exit 1
 fi
-if [[ $(timedatectl show -p NTPSynchronized --value) != "yes" ]]; then
-    echo "Setting NTP sync to true..."
-    if ! timedatectl set-ntp true; then
-        echo "[ Failed to set NTP sync to true (Sync clock). ]"
+
+
+if [[ "$(timedatectl show -p NTPSynchronized --value)" != "yes" ]]; then
+    echo "> Setting 'NTP sync' to 'true'"
+    timedatectl set-ntp true
+    if [ "${?}" -ne 0 ]; then
         exit 1
     fi
 fi
+
+
 if ! command -v fzf > /dev/null; then
-    echo "Installing 'fzf'..."
-    if ! sudo pacman -Sy fzf --noconfirm; then
-        echo "[ Failed to install 'fzf'. ]"
+    echo "> Installing 'fzf'"
+    sudo pacman -Sy fzf --noconfirm
+    if [ "${?}" -ne 0 ]; then
         exit 1
     fi
 fi
-# drive - re order to arch wiki in order of "first needed"
-working_drive="$(lsblk -ndo path,size | fzf --prompt "Select drive: " | awk '{print $1}')"
-clear
+
+
+working_drive="$(lsblk -ndo path,size | fzf --reverse --prompt "Select drive: " | awk '{print $1}')"
+wipe_drive_resonse=$(echo -e "Exit\nIRREVERSIBLY wipe '${working_drive}'" | fzf --reverse --prompt "Select option: ")
+if [[ "${wipe_drive_resonse}" != "IRREVERSIBLY wipe '${working_drive}'" ]]; then
+    exit 1
+fi
 if [[ "${working_drive}" =~ "nvme" ]]; then
     working_drive_suffix="p"
 else
     working_drive_suffix=""
 fi
-# username
+if [ -z "${working_drive}" ]; then
+    echo "! No drive selected"
+    exit 1
+fi
+clear
+
+
 while true; do
     read -p "Enter username: " username_var && echo
     clear
     case "${username_var}" in
-        "" )
-            echo "Can not be blank."
+        "")
+            echo "! Can not be blank"
             ;;
-        *[!a-zA-Z0-9]* )
-            echo "No special characters."
+        *[!a-zA-Z0-9]*)
+            echo "! No special characters"
             ;;
-        *[a-zA-Z0-9]* )
+        *[a-zA-Z0-9]*)
             break
             ;;
     esac
 done
-# password
+if [ -z "${username_var}" ]; then
+    echo "! No username provided"
+    exit 1
+fi
+clear
+
+
 while true; do
     read -p "Enter password: " password_var
     clear
     read -p "Confirm password: " password_var_confirm
     clear
     case "${password_var}" in
-        "" )
-            echo "Can not be blank."
+        "")
+            echo "! Can not be blank"
             ;;
-        "${password_var_confirm}" )
+        "${password_var_confirm}")
             break
             ;;
-        * )
-            echo "Passwords do not match."
+        *)
+            echo "! Passwords do not match"
             ;;
     esac
 done
-# timezone
-timezone_var="$(timedatectl show -p Timezone --value)"
-# microcode
-cpu_vendor="$(grep /proc/cpuinfo -e "vendor_id" | head --line 1 | awk '{print $3}')"
-#hostname
+if [ -z "${password_var}" ]; then
+    echo "! No password provided"
+    exit 1
+fi
+clear
+
+
 while true; do
     read -p "Enter hostname: " hostname_var
     clear
     case "${hostname_var}" in
-        "" )
-            echo "Can not be blank."
+        "")
+            echo "! Can not be blank"
             ;;
-        *[!a-zA-Z0-9]* )
-            echo "No special characters."
+        *[!a-zA-Z0-9]*)
+            echo "! No special characters"
             ;;
-        *[a-zA-Z0-9]* )
+        *[a-zA-Z0-9]*)
             break
             ;;
     esac
 done
+if [ -z "${hostname_var}" ]; then
+    echo "! No hostname provided"
+    exit 1
+fi
+clear
+
+
+timezone_var=$(echo -e "$(timedatectl show -p Timezone --value)\nOther" | fzf --reverse --prompt "Select timezone: ")
+if [[ "${timezone_var}" == "Other" ]]; then
+    timezone_var=$(timedatectl list-timezones | fzf --reverse --prompt "Select timezone: ")
+fi
+if [ -z "${timezone_var}" ]; then
+    echo "! Could not determine timezone"
+    exit 1
+fi
+clear
+
+
+cpu_vendor="$(grep /proc/cpuinfo -e "vendor_id" | head --line 1 | awk '{print $3}')"
+case "${cpu_vendor}" in
+    "GenuineIntel")
+        ucode_package="intel-ucode"
+        ;;
+    "AuthenticAMD")
+        ucode_package="amd-ucode"
+        ;;
+    *)
+        echo "! Could not determine microcode"
+        exit 1
+        ;;
+esac
+clear
+
+
+working_drive_text="Working drive: ${working_drive}"
+echo "${working_drive_text}"
+printf "%${#working_drive_text}s\n" | tr " " "-"
+echo "CPU Vendor: ${cpu_vendor}"
+echo "Username: ${username_var}"
+echo "Password: ${password_var}"
+echo "Hostname: ${hostname_var}"
+echo "Timezone: ${timezone_var}"
+
+
+read -p "Is this correct (y/N)?: " confirm_info_response
+case "${confirm_info_response}" in
+    [Yy]*)
+        # continue
+        ;;
+    *)
+        exit 0
+        ;;
+esac
 
 
 ##############################################
@@ -102,21 +190,9 @@ done
 # | .__/ \__,_|_|   \__|_|\__|_|\___/|_| |_| #
 # |_|                                        #
 ##############################################
-while true; do
-    read -p "This will IRREVERSIBLY wipe ${working_drive}. Continue (y/N)?" response
-    clear
-    case "${response}" in
-        [Yy]* )
-            break
-            ;;
-        * )
-            exit 0
-            ;;
-    esac
-done
 sgdisk --zap-all "${working_drive}"
 sgdisk --new=1:0:+1G --change-name=1:"boot" --typecode=1:ef00 "${working_drive}"
-sgdisk --new=2:0:+4G --change-name=2:"swap" --typecode=2:8200 "${working_drive}"
+sgdisk --new=2:0:+2G --change-name=2:"swap" --typecode=2:8200 "${working_drive}"
 sgdisk --new=3:0:0 --change-name=3:"root" --typecode=3:8300 "${working_drive}"
 
 
@@ -154,19 +230,20 @@ swapon "${working_drive}${working_drive_suffix}2"
 # | .__/ \__,_|\___|___/\__|_|  \__,_| .__/  #
 # |_|                                |_|     #
 ##############################################
+echo "> Running 'reflector'"
 reflector --latest 6 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
-case "${cpu_vendor}" in
-    "GenuineIntel" )
-        pacstrap -K /mnt intel-ucode
-        ;;
-    "AuthenticAMD" )
-        pacstrap -K /mnt amd-ucode
-        ;;
-    * )
-        echo "Could not determine microcode. Skipping."
-        ;;
-esac
-pacstrap -K /mnt base linux-lts linux-firmware linux-lts-headers bash-completion base-devel vim networkmanager grub efibootmgr
+pacstrap -K /mnt \
+base \
+base-devel \
+bash-completion \
+efibootmgr \
+grub \
+linux-firmware \
+linux-lts \
+linux-lts-headers \
+networkmanager \
+vim \
+"${ucode_package}"
 
 
 ############################
